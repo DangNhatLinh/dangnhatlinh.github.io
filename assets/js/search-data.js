8201d0c7f3764691c7af6d61c1881fd2287994b0
DangNhatLinh <lizdang2006@gmail.com>
2025-12-10 03:12:07 +0000

    Deploying to gh-pages from @ DangNhatLinh/dangnhatlinh.github.io@436837dc2c68a4002b8acc07ae8c37c9546a9ba5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,9 +402,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-automl-retraining-pipeline",
+          title: 'AutoML Retraining Pipeline',
+          description: "An automated Airflow pipeline for ETL, retraining, evaluation, and deployment, reducing manual ops and improving model reliability.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
